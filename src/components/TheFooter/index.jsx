ad2b58bf90b7e4dfd43f7a0dca012d6946642412
Felipe Stefani <felipestefani@MacBook-Pro-de-Felipe.local>
2023-08-12 22:25:33 -0300

    create The Footer component

---
+++ src/components/TheFooter/index.jsx
@@ -0,0 +1,12 @@
+import style from './style.module.css'
+
+const TheFooter = () => {
+    return (
+        <footer className="attribution">
+            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+            Coded by <a href="https://www.linkedin.com/in/felipe-stefani-a35185116/" target="_blank">Felipe Stefani</a>.
+        </footer>
+    )
+}
+
+export default TheFooter

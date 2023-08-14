e4d76365bcb1988a44bb727b31407280bd5f0810
Felipe Stefani <felipestefani@MacBook-Pro-de-Felipe.local>
2023-08-14 16:00:23 -0300

    fix: use context variable instead local variable

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,25 @@ import TheFooter from "./components/TheFooter"
 import NavigationBar from "./components/NavigationBar";
 import DisplaySection from "./components/DisplaySection";
 import ProductInfo from "./components/ProductInfo";
+import MainProvider from "./contexts/MainContext";
 
 const App = () => {
     return (
-        <div className="app_container">
-            <NavigationBar />
-            <hr />
-            <main>
-                <section>
-                    <DisplaySection />
-                </section>
-                <section>
-                    <ProductInfo />
-                </section>
-            </main>
-            <TheFooter />
-        </div>
+        <MainProvider>
+            <div className="app_container">
+                <NavigationBar />
+                <hr />
+                <main>
+                    <section>
+                        <DisplaySection />
+                    </section>
+                    <section>
+                        <ProductInfo />
+                    </section>
+                </main>
+                <TheFooter />
+            </div>
+        </MainProvider>
     )
 }
 

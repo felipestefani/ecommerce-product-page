48895215fbdde567eaa7b7130f8873d87e175cc4
Felipe Stefani <felipestefani@MacBook-Pro-de-Felipe.local>
2023-08-13 21:53:17 -0300

    create product info component

---
+++ src/components/ProductInfo/index.jsx
@@ -0,0 +1,37 @@
+import style from "./style.module.css";
+import minus from "../../assets/images/icon-minus.svg";
+import plus from "../../assets/images/icon-plus.svg";
+import cart from "../../assets/images/icon-cart.svg";
+import { useState } from "react";
+
+const ProductInfo = () => {
+
+    const [quantity, setQuantity] = useState(0) 
+    const increase = () => setQuantity(count => count + 1)
+    const decrease = () => { quantity > 0 ? setQuantity( count => count -1) : '' }
+
+
+    return (
+        <div className={style.product_info_container}> 
+            <h2 className={style.company_name}>Sneaker Company</h2>     
+            <h1 className={style.product_title}>Fall Limited Edition Sneakers</h1>
+            <p className={style.product_text_info}>These low-profile sneakers are your perfect casual wear companion. Featuring a 
+            durable rubber outer sole, they’ll withstand everything the weather can offer.</p>
+            <div className={style.price}>
+                <p className={style.after_discount}>$125.00 <span className={style.discount}>50%</span></p>
+                <p className={style.before_discount}>$250.00</p>
+            </div>
+            <div className={style.action_section}>
+                <div className={style.quantity}>
+                    <img className={style.minus} src={minus} alt="minus icon"  onClick={decrease}/>
+                    <span className={style.quantity_value}>{quantity}</span>
+                    <img className={style.plus} src={plus} alt="plus icon" onClick={increase}/>
+                </div>
+                <button className={style.add_to_cart}><img className={style.cart_icon} src={cart} alt="cart icon" /> Add to cart</button>
+            </div>
+
+        </div>
+    )
+}
+
+export default ProductInfo

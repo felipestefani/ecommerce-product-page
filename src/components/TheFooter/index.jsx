980ce22691692ecf75842c4243268cba21edee33
Felipe Stefani <felipestefani@MacBook-Pro-de-Felipe.local>
2023-08-14 15:31:21 -0300

    fix: footer font size

--- src/components/TheFooter/index.jsx
+++ src/components/TheFooter/index.jsx
@@ -2,7 +2,7 @@ import style from './style.module.css'
 
 const TheFooter = () => {
     return (
-        <footer className="attribution">
+        <footer className={style.attribution}>
             Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
             Coded by <a href="https://www.linkedin.com/in/felipe-stefani-a35185116/" target="_blank">Felipe Stefani</a>.
         </footer>

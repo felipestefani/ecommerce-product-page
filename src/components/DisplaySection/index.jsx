315b4c73dca74bc735cd3cd0849ce9e34faa8ede
Felipe Stefani <felipestefani@MacBook-Pro-de-Felipe.local>
2023-08-13 21:51:26 -0300

    fix: media querie and some component issues

--- src/components/DisplaySection/index.jsx
+++ src/components/DisplaySection/index.jsx
@@ -3,15 +3,45 @@ import product_1 from '../../assets/images/image-product-1.jpg'
 import product_2 from '../../assets/images/image-product-2.jpg'
 import product_3 from '../../assets/images/image-product-3.jpg'
 import product_4 from '../../assets/images/image-product-4.jpg'
-
+import thumb_1 from '../../assets/images/image-product-1-thumbnail.jpg'
+import thumb_2 from '../../assets/images/image-product-2-thumbnail.jpg'
+import thumb_3 from '../../assets/images/image-product-3-thumbnail.jpg'
+import thumb_4 from '../../assets/images/image-product-4-thumbnail.jpg'
+import { useState } from "react";
 
 const DisplaySection = () => {
+
+    const images = [product_1, product_2, product_3, product_4]
+    const thumbnails = [thumb_1, thumb_2, thumb_3, thumb_4]
+    const [mainImage, setMainImage] = useState(product_1)
+    const [thumbImage, setThumbImage] = useState('')
+
+    const changeToImage = (index,thumb) => {
+        setMainImage(images[index])
+        setThumbImage(thumb)
+    }
+
     return (
-        <div className="display_section_container">
-            <img className={style.product_image} src={product_1} alt="product one"/>
-            <img className={style.product_image} src={product_2} alt="product two"/>
-            <img className={style.product_image} src={product_3} alt="product three"/>
-            <img className={style.product_image} src={product_4} alt="product four"/>
+        <div className={style.display_section_container}>
+            <div className={style.main_image}>
+                <img className={style.product_image} src={mainImage} alt="main product image"/>    
+            </div>
+            <div className={`${style.thumbnails} ${style.display_thumbnails}`}>
+                {
+                    thumbnails.map( (thumb, index) => (        
+                            <div key={thumb+index} className={style.thumb_container} >
+                                <div className={thumbImage == thumb ? style.emphasis_thumb : ''} style={{ display:thumbImage == thumb ? 'block' : 'none'}}></div>
+                                <img
+                                    key={thumb} 
+                                    className={`${style.thumbnail_image}`} 
+                                    src={thumb} 
+                                    alt="product thumbnail image" 
+                                    onClick={() => changeToImage(index, thumb)}
+                                />
+                            </div>
+                    ))
+                }
+            </div>
         </div>
     )
 }
